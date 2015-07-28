b86ab31b1f5e47f9e38b51d6db0396e18643a1c9
Jared Porter <jared.r.porter@gmail.com>
2015-07-28 22:27:13 +0100

    Made a Sieve to import from instead of using Project 7 all the time.

---
+++ SieveOfEratosthenes.py
@@ -0,0 +1,16 @@
+mport itertools
+
+def sieveOfEra():
+"""Create a generator for primes using the Sieve of Eratosthenes."""
+    D = {}
+    yield 2
+    for q in itertools.islice(itertools.count(3), 0, None, 2):
+        p = D.pop(q, None)
+        if p is None:
+            D[q*q] = q
+            yield q
+        else:
+            x = p + q
+            while x in D or not (x&1):
+                x += p
+            D[x] = p
